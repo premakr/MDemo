e90bf0055d7e6c541cc0800771b9789958da4b7e
premakr <premakr86@gmail.com>
2020-03-12 16:08:32 +0530

    mi

---
+++ src/test/java/org/tyss/MyMavenProject/Demo1.java
@@ -0,0 +1,9 @@
+package org.tyss.MyMavenProject;
+import org.testng.annotations.Test;
+public class Demo1 {
+	@Test
+	public void test() {
+		System.out.println("maven executing2.....");
+	}
+
+}
---
+++ src/test/java/org/tyss/MyMavenProject/demoB.java
@@ -0,0 +1,12 @@
+package org.tyss.MyMavenProject;
+
+import org.testng.annotations.Test;
+
+public class demoB {
+	@Test
+	public void test() {
+		System.out.println("maven executing1.....");
+	}
+
+
+}
